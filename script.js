429e723a8f2512c7104c0f073f31216f607e0d15
AntCannon <anthonycannonier@gmail.com>
2024-03-17 10:04:51 -0400

    get table row nodes

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// giantbomb api = "81e314bb98e0e727dea2b08bd4e37b0099ed7ad8"
-
-// giantapi documentation = https://www.giantbomb.com/api/
-
-console.log("Hello World")
-
 // game availability object
 // const inventory = new Map([
 //   ["Helldivers 2", new Map([
@@ -25,4 +19,19 @@ const lendForm = document.getElementById("lend-form");
 lendForm.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(lendForm);
+})
+
+// create table row
+const inventoryBody = document.getElementById("inventory-body")
+
+const borrowButton = document.querySelector(".borrow-button")
+borrowButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const row = borrowButton.parentNode.parentNode;
+  const platform = (row.querySelector(".platform"));
+  const gameTitle = (row.querySelector(".game-title"));
+  const gameEdition = (row.querySelector(".game-edition"));
+  const stock = (row.querySelector(".stock"));
+  const out = (row.querySelector(".out"));
+  const expectedAvailability = (row.querySelector(".expected-availability"));
 })
