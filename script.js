0dffdea536ba406a709e6f29a55b844e1726cfd5
AntCannon <anthonycannonier@gmail.com>
2024-03-17 14:03:11 -0400

    get addGameForm and inventory body nodes

---
+++ script.js
@@ -0,0 +1,14 @@
+// get form data
+const addGameForm = document.getElementById("add-game-form")
+console.log(addGameForm)
+
+
+// get table body
+const tableBody = document.getElementById("inventory-body")
+console.log(tableBody)
+
+
+// create new row
+const inventoryRow = document.createElement("tr");
+
+// create cells
