f94d6a42ad0b5797daaf637a9476011e9462436a
AntCannon <anthonycannonier@gmail.com>
2024-03-17 15:41:48 -0400

    add addListenertoRemoveButton()

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ const tableBody = document.getElementById("inventory-body")
 console.log(tableBody)
 
 
+// add remove button listeners to existing remove buttons
+
+function addListenerToRemoveButtons() {
+  const removeButtons = tableBody.querySelectorAll(".remove-button")
+  for (let removeButton of removeButtons) {
+    removeButton.addEventListener("click", (e) => {
+      console.log("remove button click")
+      removeButton.parentNode.parentNode.remove()
+    })
+  }
+}
+
+addListenerToRemoveButtons();
+
+
 // create new row
 const inventoryRow = document.createElement("tr");
 
