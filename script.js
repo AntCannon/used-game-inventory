a754df41d7e55f99a935991062b260c40418e040
AntCannon <anthonycannonier@gmail.com>
2024-03-18 22:12:11 -0400

    add stock increment and decrement functionality witih total update

--- script.js
+++ script.js
@@ -42,6 +42,13 @@ const tableColumns = ["platform", "game-title", "game-edition", "stock-cell", "o
 function addInventoryRow (platform, gameTitle, edition, stock) {
   // create new row
   const inventoryRow = document.createElement("tr");
+
+  // variables for buttons
+  let decrementButton;
+  let stockSpan;
+  let incrementButton;
+  let out;
+  let total;
   
   // create cells
   for (let elClass of tableColumns) {
@@ -57,41 +64,52 @@ function addInventoryRow (platform, gameTitle, edition, stock) {
       cell.innerText = edition
     }
     if (elClass === "stock-cell") {
-      createStockDecrementButton(cell)
-      createStockSpan(cell, stock)
-      createStockIncrementButton(cell)
+      decrementButton = createStockDecrementButton(cell);
+      stockSpan  = createStockSpan(cell, stock);
+      incrementButton = createStockIncrementButton(cell);
+
+      addListenerToDecrementButton(decrementButton, stockSpan);
+      addListenerToIncrementButton(incrementButton, stockSpan);
     }
 
     if (elClass === "out") {
-      cell.innerText = 0
+      out = cell
+      out.innerText = 0;
     }
 
     if (elClass === "total") {
-      cell.innerText = stock
+      total = cell
+      total.innerText = stock;
     }
 
     if (elClass === "remove") {
       createRowRemoveButton(cell);
     }
 
-    inventoryRow.appendChild(cell)
+    inventoryRow.appendChild(cell);
   }
+
+  addListenerUpdateTotalToStockButtons(decrementButton, incrementButton, stockSpan, out, total);
+
   tableBody.appendChild(inventoryRow);
 }
 
 // create row remove eventListener function
 function addListenerToRowRemoveButton(node) {
   node.addEventListener("click", (e) => {
-    node.parentNode.parentNode.remove()
+    node.parentNode.parentNode.remove();
   })
 }
 
 // create stock decrement button
 function createStockDecrementButton(node) {
   const decrementButton = document.createElement("button");
+  decrementButton.setAttribute("type", "button");
   decrementButton.classList.add("decrease-stock")
   decrementButton.innerText = "-"
-  node.appendChild(decrementButton)
+  node.appendChild(decrementButton);
+
+  return decrementButton;
 }
 
 // create stock span
@@ -100,14 +118,39 @@ function createStockSpan(node, stock) {
   span.classList.add("stock");
   span.innerText = stock;
   node.appendChild(span)
+
+  return span;
+}
+
+// add listener to stock decrement button
+function addListenerToDecrementButton(button, stockSpan) {
+  button.addEventListener("click", () => {
+    let currentStock = +stockSpan.innerText
+    if (currentStock > 0) {
+      stockSpan.innerText = --currentStock;
+    }
+  })
 }
 
 // create stock increment button
 function createStockIncrementButton(node) {
   const incrementButton = document.createElement("button");
+  incrementButton.setAttribute("type", "button");
   incrementButton.classList.add("increase-stock")
   incrementButton.innerText = "+"
-  node.appendChild(incrementButton)
+  node.appendChild(incrementButton);
+
+  return incrementButton;
+}
+
+// add listener to stock increment button
+function addListenerToIncrementButton(button, stockSpan) {
+  button.addEventListener("click", () => {
+    let currentStock = +stockSpan.innerText
+    if (currentStock < 99) {
+      stockSpan.innerText = ++currentStock;
+    }
+  })
 }
 
 // create inventory row remove button
@@ -117,4 +160,20 @@ function createRowRemoveButton(node) {
   rowRemoveButton.innerText = "Remove";
   addListenerToRowRemoveButton(rowRemoveButton)
   node.appendChild(rowRemoveButton)
+}
+
+// add updateTotal listener to increment and decrement buttons
+function addListenerUpdateTotalToStockButtons(decrement, increment, stockSpan, out, total) {
+  decrement.addEventListener("click", (e) => {
+    updateTotal(stockSpan, out, total);
+  })
+  increment.addEventListener("click", (e) => {
+    updateTotal(stockSpan, out, total);
+  })
+}
+
+// update total
+function updateTotal(stockSpan, out, total) {
+  const newTotal = +stockSpan.innerText + +out.innerText;
+  total.innerText = newTotal;
 }
